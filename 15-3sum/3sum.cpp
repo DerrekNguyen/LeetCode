e2e07ea71fb43929856a9e59d85e7a4c344381ef
Minh Nhat (Derrek) Nguyen <143032197+DerrekNguyen@users.noreply.github.com>
2025-09-28 19:04:27 -0400

    Time: 43 ms (86.70%) | Memory: 29 MB (89.70%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -14,8 +14,8 @@ public:
                 else if (sum < 0) l++;
                 else if (sum == 0) {
                     result.push_back({nums[i], nums[l], nums[r]});
-                    l++;
-                    while (l < r && nums[l] == nums[l - 1]) ++l;
+                    r--;
+                    while (l < r && nums[r] == nums[r + 1]) r--;
                 }
             }
         }
